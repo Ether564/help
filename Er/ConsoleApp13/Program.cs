1a4098f90eb93bb80159095d5278efc682cde48b
Ether564 <qwsx.er9@gmail.com>
2025-06-27 11:14:45 +0300

    Add files via upload

---
+++ Er/ConsoleApp13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите тип документа (xml, txt, doc): ");
+            string type = Console.ReadLine();
+
+            AbstractHandler handler;
+
+            if (type == "xml")
+            {
+                handler = new XMLHandler();
+            }
+            else if (type == "txt")
+            {
+                handler = new TXTHandler();
+            }
+            else if (type == "doc")
+            {
+                handler = new DOCHandler();
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный формат");
+                return;
+            }
+
+            handler.Open();
+            handler.Create();
+            handler.Chenge();
+            handler.Save();
+
+            Console.ReadKey();
+        }
+    }
+}
